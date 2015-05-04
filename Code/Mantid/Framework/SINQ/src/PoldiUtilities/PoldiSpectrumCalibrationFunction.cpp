c3295ac95f13f1aa45f441cc97842dea6a601527
Michael Wedel <michael.wedel@psi.ch>
2015-05-04 07:09:02 +0200

    Refs #11674. Checkpointing work

---
+++ Code/Mantid/Framework/SINQ/src/PoldiUtilities/PoldiSpectrumCalibrationFunction.cpp
@@ -0,0 +1,21 @@
+#include "MantidSINQ/PoldiUtilities/PoldiSpectrumCalibrationFunction.h"
+
+namespace Mantid
+{
+namespace Poldi
+{
+
+PoldiSpectrumCalibrationFunction::PoldiSpectrumCalibrationFunction() :
+    PoldiSpectrumDomainFunction()
+{
+
+}
+
+PoldiSpectrumCalibrationFunction::getPeakCenter(const Poldi2DHelper_sptr &poldi2DHelper) const
+{
+
+}
+
+
+} // namespace Poldi
+} // namespace Mantid
---
+++ Code/Mantid/Framework/SINQ/test/PoldiSpectrumCalibrationFunctionTest.h
@@ -0,0 +1,29 @@
+#ifndef MANTID_SINQ_POLDISPECTRUMCALIBRATIONFUNCTIONTEST_H_
+#define MANTID_SINQ_POLDISPECTRUMCALIBRATIONFUNCTIONTEST_H_
+
+#include <cxxtest/TestSuite.h>
+
+#include "MantidSINQ/PoldiUtilities/PoldiSpectrumCalibrationFunction.h"
+
+using Mantid::SINQ::PoldiSpectrumCalibrationFunction;
+using namespace Mantid::API;
+
+class PoldiSpectrumCalibrationFunctionTest : public CxxTest::TestSuite
+{
+public:
+  // This pair of boilerplate methods prevent the suite being created statically
+  // This means the constructor isn't called when running other tests
+  static PoldiSpectrumCalibrationFunctionTest *createSuite() { return new PoldiSpectrumCalibrationFunctionTest(); }
+  static void destroySuite( PoldiSpectrumCalibrationFunctionTest *suite ) { delete suite; }
+
+
+  void test_Something()
+  {
+    TSM_ASSERT( "You forgot to write a test!", 0);
+  }
+
+
+};
+
+
+#endif /* MANTID_SINQ_POLDISPECTRUMCALIBRATIONFUNCTIONTEST_H_ */
