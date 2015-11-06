a78dedbf3168de34297e4236ad208eec6e103335
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-11-06 10:01:54 +0000

    Changed formatting in CreateVesuvioIDF alg
    Refs #14305

--- Framework/PythonInterface/plugins/algorithms/CreateVesuvioIDF.py
+++ Framework/PythonInterface/plugins/algorithms/CreateVesuvioIDF.py
@@ -73,30 +73,30 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_header(self):
         # Define the header of the IDF
-        idf_header = ("""<?xml version="1.0" encoding="UTF-8"?>
-        <!-- For help on the notation used to specify an Instrument Definition File
-             see http://www.mantidproject.org/IDF -->
-        <instrument xmlns="http://www.mantidproject.org/IDF/1.0"
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                    xsi:schemaLocation="http://www.mantidproject.org/IDF/1.0 http://schema.mantidproject.org/IDF/1.0/IDFSchema.xsd"
-                    name="VESUVIO"
-                    valid-from="2006-08-11 00:00:00"
-                    valid-to="2100-01-31 23:59:59"
-                    last-modified="2011-07-20 00:00:21">
-
-        <defaults>
-            <length unit="meter"/>
-            <angle unit="degree"/>
-            <reference-frame>
-              <!-- The z-axis is set parallel to and in the direction of the beam. the
-                   y-axis points up and the coordinate system is right handed. -->
-              <along-beam axis="z"/>
-              <pointing-up axis="y"/>
-              <handedness val="right"/>
-            </reference-frame>
-        </defaults>
-
-        """)
+        idf_header = (
+        '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        '<!-- For help on the notation used to specify an Instrument Definition File\n' +
+        '     see http://www.mantidproject.org/IDF -->\n' +
+        '<instrument xmlns="http://www.mantidproject.org/IDF/1.0"\n' +
+        '            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
+        '            xsi:schemaLocation="http://www.mantidproject.org/IDF/1.0 http://schema.mantidproject.org/IDF/1.0/IDFSchema.xsd"\n' +
+        '            name="VESUVIO"\n' +
+        '            valid-from="2006-08-11 00:00:00"\n' +
+        '            valid-to="2100-01-31 23:59:59"\n' +
+        '            last-modified="2011-07-20 00:00:21">\n' +
+        '\n' +
+        '<defaults>\n' +
+        '   <length unit="meter"/>\n' +
+        '   <angle unit="degree"/>\n' +
+        '   <reference-frame>\n' +
+        '      <!-- The z-axis is set parallel to and in the direction of the beam. the\n' +
+        '           y-axis points up and the coordinate system is right handed. -->\n' +
+        '      <along-beam axis="z"/>\n' +
+        '      <pointing-up axis="y"/>\n' +
+        '      <handedness val="right"/>\n' +
+        '   </reference-frame>\n' +
+        '</defaults>' +
+        '\n')
 
         return idf_header
 
@@ -104,23 +104,22 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_source_sample(self, non_monitor_L0):
         # Define the source and sample positions
-        idf_src_sam = ("""
-          <!--  SOURCE AND SAMPLE POSITION -->
-
-          <component type="moderator">
-            <location z="-%s" />
-          </component>
-
-          <type name="moderator" is="Source" />
-
-          <component type="sample-position">
-            <location />
-          </component>
-
-          <type name="sample-position" is="SamplePos">
-          </type>
-
-        """ % non_monitor_L0)
+        idf_src_sam = (
+        '  <!--  SOURCE AND SAMPLE POSITION -->\n'+
+        '\n' +
+        '  <component type="moderator">\n' +
+        ('    <location z="-%s" />\n' % non_monitor_L0) +
+        '  </component>\n' +
+        '\n' +
+        '  <type name="moderator" is="Source" />\n' +
+        '\n' +
+        '  <component type="sample-position">\n' +
+        '    <location />\n' +
+        '  </component>\n' +
+        '\n' +
+        '  <type name="sample-position" is="SamplePos">\n' +
+        '  </type>\n' +
+        '\n')
 
         return idf_src_sam
 
@@ -128,24 +127,23 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_monitors(self, monitor_one, monitor_two):
         # Define the Monitors
-        idf_monitors = ("""
-          <!-- MONITORS -->
-
-          <component type="monitor" idlist="monitors">
-            <location z="%s" name="Monitor 1"/>
-            <location z="%s" name="Monitor 2"/>
-          </component>
-
-          <type name="monitor" is="monitor">
-           <cuboid id="shape">
-              <left-front-bottom-point x="0.01" y="-0.01" z="0.0"  />
-              <left-front-top-point  x="0.01" y="-0.01" z="0.02"  />
-              <left-back-bottom-point  x="-0.01" y="-0.01" z="0.0"  />
-              <right-front-bottom-point  x="0.01" y="0.01" z="0.0"  />
-           </cuboid>
-          </type>
-
-        """ % (monitor_one[5], monitor_two[5]))
+        idf_monitors = (
+        '  <!-- MONITORS -->\n' +
+        '\n' +
+        '  <component type="monitor" idlist="monitors">\n' +
+        ('    <location z="%s" name="Monitor 1"/>\n' % monitor_one[5]) +
+        ('    <location z="%s" name="Monitor 2"/>\n' % monitor_two[5]) +
+        '  </component>\n' +
+        '\n' +
+        '  <type name="monitor" is="monitor">\n' +
+        '   <cuboid id="shape">\n' +
+        '      <left-front-bottom-point x="0.01" y="-0.01" z="0.0"  />\n' +
+        '      <left-front-top-point  x="0.01" y="-0.01" z="0.02"  />\n' +
+        '      <left-back-bottom-point  x="-0.01" y="-0.01" z="0.0"  />\n' +
+        '      <right-front-bottom-point  x="0.01" y="0.01" z="0.0"  />\n' +
+        '   </cuboid>\n' +
+        '  </type>\n' +
+        '\n')
 
         return idf_monitors
 
@@ -153,79 +151,77 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_foils(self):
         # Define the Foils and Foil changers
-        idf_foils = ("""
-          <!-- FOILS  -->
-
-          <!-- midpoint positions
-               phi is simply set to put them all in the scattering plane
-          -->
-          <component name="foil-pos0" type="foil">
-           <!-- position 0 -->
-           <location t="-42" r="0.225" p="0"/>
-           <location t="-64" r="0.225" p="0"/>
-           <location t="35" r="0.225" p="0"/>
-           <location t="55" r="0.225" p="0"/>
-          </component>
-
-          <component name="foil-pos1" type="foil">
-           <!-- position 1 -->
-           <location t="-31" r="0.225" p="0"/>
-           <location t="-50" r="0.225" p="0"/>
-           <location t="46" r="0.225" p="0"/>
-           <location t="67" r="0.225" p="0"/>
-          </component>
-
-          <!-- The foil shape is currently set to give the correct dTheta as seen
-               from the sample position -->
-          <type name="foil">
-            <cuboid id="foil-shape-shape">
-              <left-front-bottom-point x="-0.02000121679" y="0.001" z="-0.001"/>
-              <left-front-top-point x="-0.02000121679" y="0.001" z="0.001"/>
-              <left-back-bottom-point x="-0.02000121679" y="-0.001" z="-0.001" />
-              <right-front-bottom-point x="0.02000121679" y="0.001" z="-0.001" />
-            </cuboid>
-          </type>
-
-          <!-- FOIL CHANGER -->
-          <!-- The shape & location are made up to provide inputs for the
-               CalculateGammaBackground algorithm -->
-          <component type="foil-changer">
-          <location x="0" y ="0" z="0"/>
-          </component>
-
-          <!-- Important quantity currently is the height. It defines the Z range
-               for the integration of the foil positions  (-0.2,0.2)-->
-          <type name="foil-changer">
-            <cylinder id="foil-changer-shape">
-            <centre-of-bottom-base x="0.0" y="-0.2" z="0.0" />
-            <axis x="0.0" y="1" z="0" />
-            <radius val="0.1" />
-            <height val="0.4" />
-            </cylinder>
-          </type>
-
-        """)
+        idf_foils = (
+        '  <!-- FOILS  -->\n' +
+        '\n' +
+        '  <!-- midpoint positions\n' +
+        '       phi is simply set to put them all in the scattering plane\n' +
+        '  -->\n' +
+        '  <component name="foil-pos0" type="foil">\n' +
+        '   <!-- position 0 -->\n' +
+        '   <location t="-42" r="0.225" p="0"/>\n' +
+        '   <location t="-64" r="0.225" p="0"/>\n' +
+        '   <location t="35" r="0.225" p="0"/>\n' +
+        '   <location t="55" r="0.225" p="0"/>\n' +
+        '  </component>\n' +
+        '\n' +
+        '  <component name="foil-pos1" type="foil">\n' +
+        '   <!-- position 1 -->\n' +
+        '   <location t="-31" r="0.225" p="0"/>\n' +
+        '   <location t="-50" r="0.225" p="0"/>\n' +
+        '   <location t="46" r="0.225" p="0"/>\n' +
+        '   <location t="67" r="0.225" p="0"/>\n' +
+        '  </component>\n' +
+        '\n' +
+        '  <!-- The foil shape is currently set to give the correct dTheta as seen\n' +
+        '       from the sample position -->\n' +
+        '  <type name="foil">\n' +
+        '    <cuboid id="foil-shape-shape">\n' +
+        '      <left-front-bottom-point x="-0.02000121679" y="0.001" z="-0.001"/>\n' +
+        '      <left-front-top-point x="-0.02000121679" y="0.001" z="0.001"/>\n' +
+        '      <left-back-bottom-point x="-0.02000121679" y="-0.001" z="-0.001" />\n' +
+        '      <right-front-bottom-point x="0.02000121679" y="0.001" z="-0.001" />\n' +
+        '    </cuboid>\n' +
+        '  </type>\n' +
+        '\n' +
+        '  <!-- FOIL CHANGER -->\n' +
+        '  <!-- The shape & location are made up to provide inputs for the\n' +
+        '       CalculateGammaBackground algorithm -->\n' +
+        '  <component type="foil-changer">\n' +
+        '  <location x="0" y ="0" z="0"/>\n' +
+        '  </component>\n' +
+        '\n' +
+        '  <!-- Important quantity currently is the height. It defines the Z range\n' +
+        '       for the integration of the foil positions  (-0.2,0.2)-->\n' +
+        '  <type name="foil-changer">\n' +
+        '    <cylinder id="foil-changer-shape">\n' +
+        '    <centre-of-bottom-base x="0.0" y="-0.2" z="0.0" />\n' +
+        '    <axis x="0.0" y="1" z="0" />\n' +
+        '    <radius val="0.1" />\n' +
+        '    <height val="0.4" />\n' +
+        '    </cylinder>\n' +
+        '  </type>\n' +
+        '\n')
         return idf_foils
 
 
 #===========================================================================================================================================#
     def _construct_idf_forward(self, ip_lines):
         # Define the forward scattering detectors
-        idf_forward_det_head = ("""
-          <!-- DETECTORS -->
-
-          <component type="forward" idlist="forward">
-            <location />
-          </component>
-
-
-          <component type="back" idlist="back">
-            <location />
-          </component>
-
-          <type name="forward">
-            <component type="pixel forward" >
-        """)
+        idf_forward_det_head = (
+        '  <!-- DETECTORS -->\n' +
+        '\n' +
+        '  <component type="forward" idlist="forward">\n' +
+        '    <location />\n' +
+        '  </component>\n' +
+        '\n' +
+        '\n' +
+        '  <component type="back" idlist="back">\n' +
+        '    <location />\n' +
+        '  </component>\n' +
+        '\n' +
+        '  <type name="forward">\n' +
+        '    <component type="pixel forward" >\n')
 
         # Define all locations of forward scattering detectors
         idf_forward_locations = ''
@@ -234,10 +230,10 @@ class CreateVesuvioIDF(PythonAlgorithm):
             ip_det = ip_lines[idx].split()
             idf_forward_locations += ('      <location t="-%s" r="%s" p="%i"/>\n' % (ip_det[2], ip_det[5], 0))
 
-        idf_forward_det_end = ("""    </component>
-          </type>
-
-        """)
+        idf_forward_det_end = (
+        '    </component>\n' +
+        '  </type>\n' +
+        '\n')
 
         # Construct forward detector definitions string
         idf_forward_dets = idf_forward_det_head + idf_forward_locations + idf_forward_det_end
@@ -247,9 +243,9 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_back(self, ip_lines):
         # Define the back scattering detectors
-        idf_back_head =("""  <type name="back">
-            <component type="pixel back" >
-        """)
+        idf_back_head =(
+        '   <type name="back">\n' +
+        '     <component type="pixel back" >\n')
 
         # Define all locations of forward scattering detectors
         idf_back_locations = ''
@@ -258,9 +254,9 @@ class CreateVesuvioIDF(PythonAlgorithm):
             ip_det = ip_lines[idx].split()
             idf_back_locations += ('      <location t="-%s" r="%s" p="%i" name="S%s"/>\n' % (ip_det[2], ip_det[5], 0, ip_det[1]))
 
-        idf_back_end = ("""    </component>
-          </type>
-        """)
+        idf_back_end = (
+        '    </component>\n' +
+        '  </type>\n')
 
         # Construct back detector definitions string
         idf_back_dets = idf_back_head + idf_back_locations + idf_back_end
@@ -270,54 +266,54 @@ class CreateVesuvioIDF(PythonAlgorithm):
 #===========================================================================================================================================#
     def _construct_idf_pixel(self):
         # Define pixel forward/back
-        idf_pix = ("""
-          <type name="pixel forward" is="detector">
-            <cuboid id="shape">
-              <left-front-bottom-point x="0.0125" y="-0.0395" z= "0.0045" />
-              <left-front-top-point x="0.0125" y="0.0395" z= "0.0045" />
-              <left-back-bottom-point x="0.0125" y="-0.0395" z= "-0.0045" />
-              <right-front-bottom-point x="-0.0125" y="-0.0395" z= "0.0045" />
-            </cuboid>
-            <algebra val="shape" />
-          </type>
+        idf_pix = (
+        '  <type name="pixel forward" is="detector">\n' +
+        '    <cuboid id="shape">\n' +
+        '      <left-front-bottom-point x="0.0125" y="-0.0395" z= "0.0045" />\n' +
+        '      <left-front-top-point x="0.0125" y="0.0395" z= "0.0045" />\n' +
+        '      <left-back-bottom-point x="0.0125" y="-0.0395" z= "-0.0045" />\n' +
+        '      <right-front-bottom-point x="-0.0125" y="-0.0395" z= "0.0045" />\n' +
+        '    </cuboid>\n' +
+        '    <algebra val="shape" />\n' +
+        '  </type>\n' +
+        '\n' +
+        '  <type name="pixel back" is="detector">\n' +
+        '    <cuboid id="shape" >\n' +
+        '      <left-front-bottom-point x="0.0125" y="-0.04" z= "0.0045" />\n' +
+        '      <left-front-top-point x="0.0125" y="0.04" z= "0.0045" />\n' +
+        '      <left-back-bottom-point x="0.0125" y="-0.04" z= "-0.0045" />\n' +
+        '      <right-front-bottom-point x="-0.0125" y="-0.04" z= "0.0045" />\n' +
+        '    </cuboid>\n' +
+        '  <algebra val="shape" />\n' +
+        '  </type>\n' +
+        '\n')
 
-          <type name="pixel back" is="detector">
-            <cuboid id="shape" >
-              <left-front-bottom-point x="0.0125" y="-0.04" z= "0.0045" />
-              <left-front-top-point x="0.0125" y="0.04" z= "0.0045" />
-              <left-back-bottom-point x="0.0125" y="-0.04" z= "-0.0045" />
-              <right-front-bottom-point x="-0.0125" y="-0.04" z= "0.0045" />
-            </cuboid>
-          <algebra val="shape" />
-          </type>
-
-        """)
         return idf_pix
 
 
 #===========================================================================================================================================#
     def _construct_idf_det_list(self):
         # Detector ID Lists + tail
-        idf_det_id_list = ("""
-          <!-- DETECTOR ID LISTS -->
+        idf_det_id_list = (
+        '  <!-- DETECTOR ID LISTS -->\n' +
+        '\n' +
+        '  <idlist idname="back">\n' +
+        '    <id start="2101" end="2144" />\n' +
+        '    <id start="2201" end="2244" />\n' +
+        '    <id start="2301" end="2344" />\n' +
+        '  </idlist>\n' +
+        '\n' +
+        '  <idlist idname="monitors">\n' +
+        '    <id start="1101" end="1102" />\n' +
+        '  </idlist>\n' +
+        '\n' +
+        '  <idlist idname="forward">\n' +
+        '    <id start="3101" end="3132" />\n' +
+        '    <id start="3201" end="3232" />\n' +
+        '  </idlist>\n' +
+        '\n' +
+        '</instrument>\n')
 
-          <idlist idname="back">
-            <id start="2101" end="2144" />
-            <id start="2201" end="2244" />
-            <id start="2301" end="2344" />
-          </idlist>
-
-          <idlist idname="monitors">
-            <id start="1101" end="1102" />
-          </idlist>
-
-          <idlist idname="forward">
-            <id start="3101" end="3132" />
-            <id start="3201" end="3232" />
-          </idlist>
-
-        </instrument>
-        """)
         return idf_det_id_list
 
 
