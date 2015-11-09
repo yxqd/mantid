fef4429d2ba9ad97061815e0fd9dd694cd0ee1a0
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2015-11-09 15:46:15 +0000

    Add test for Theta and R values in unit test
    Refs #14305

--- Framework/PythonInterface/test/python/plugins/algorithms/CreateVesuvioIDFTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/CreateVesuvioIDFTest.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 from mantid.simpleapi import *
 from IndirectImport import *
 from mantid.api import MatrixWorkspace, WorkspaceGroup
@@ -28,12 +29,17 @@ class CreateVesuvioIDFTest(unittest.TestCase):
         Test will compare z values
         Due to current lack of Phi value, x and y can not be checked as they depend on Phi)
         """
-        # Check instrument
-        self.assertEquals(self._new_definition.getFullName(), 'VESUVIO')
-        # Check number of detectors are equal
+        # Obtain sample position and detector number for old/new files
+        new_sample_pos = self._new_definition.getInstrument()
+        old_sample_pos = self._old_definition.getInstrument()
         new_length = self._new_definition.getNumberHistograms()
         old_length = self._old_definition.getNumberHistograms()
+
+        # Check Instrument is correct
+        self.assertEquals(self._new_definition.getFullName(), 'VESUVIO')
+        # Check number of detectors are equal
         self.assertEquals(new_length, old_length)
+
         # Loop over all detectors
         for i in range(0, new_length):
             # Get new/old detectors
@@ -43,6 +49,15 @@ class CreateVesuvioIDFTest(unittest.TestCase):
             self.assertEquals(round(new_det.getPos()[2], 5), round(old_det.getPos()[2], 5))
             # Check for equality of IDs
             self.assertEquals(new_det.getID(), old_det.getID())
+            # Check R values
+            new_r = new_det.getDistance(new_sample_pos)
+            old_r = old_det.getDistance(old_sample_pos)
+            self.assertEquals(new_r, old_r)
+            # Check Theta values
+            new_theta = ((self._new_definition.detectorTwoTheta(new_det) * 180) / math.pi)
+            old_theta = ((self._old_definition.detectorTwoTheta(old_det) * 180) / math.pi)
+            self.assertEquals(new_theta, old_theta)
+
 
     def test_monitors(self):
         """
