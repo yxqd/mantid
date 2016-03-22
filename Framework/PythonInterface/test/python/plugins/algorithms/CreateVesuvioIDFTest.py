3bc53d13dc2d23b2ffdbd68511742c976300f7b5
Elliot Oram <Elliot.Oram@stfc.ac.uk>
2016-03-22 09:00:36 +0000

    Get Test to run correctly
    Refs #14305

--- Framework/PythonInterface/test/python/plugins/algorithms/CreateVesuvioIDFTest.py
+++ Framework/PythonInterface/test/python/plugins/algorithms/CreateVesuvioIDFTest.py
@@ -6,8 +6,8 @@ from mantid.api import MatrixWorkspace, WorkspaceGroup
 
 class CreateVesuvioIDFTest(unittest.TestCase):
 
-    _ip_filename = 'path_to_ip.par'
-    _idf_filename = 'path_to_idf.xml'
+    _ip_filename = 'Vesuvio_IP_file_test.par'
+    _idf_filename = 'generated_idf.xml'
     _old_definition = None
     _new_definition = None
 
@@ -20,8 +20,10 @@ class CreateVesuvioIDFTest(unittest.TestCase):
         CreateVesuvioIDF(IPFilename=self._ip_filename,
                          OutputFilename=self._idf_filename)
         # Get Definitions
-        self._old_definition = LoadEmptyVesuvio(InstrumentParFile=self._ip_filename)
-        self._new_definition = LoadEmptyInstrument(Filename=self._idf_filename)
+        LoadEmptyVesuvio(InstrumentParFile=self._ip_filename, OutputWorkspace='old_idf')
+        LoadEmptyInstrument(Filename=self._idf_filename, OutputWorkspace='new_idf')
+        self._old_definition = mtd['old_idf']
+        self._new_definition = mtd['new_idf']
 
     def test_generated_values(self):
         """
@@ -30,13 +32,12 @@ class CreateVesuvioIDFTest(unittest.TestCase):
         Due to current lack of Phi value, x and y can not be checked as they depend on Phi)
         """
         # Obtain sample position and detector number for old/new files
-        new_sample_pos = self._new_definition.getInstrument()
-        old_sample_pos = self._old_definition.getInstrument()
+        new_instrument = self._new_definition.getInstrument()
+        old_instrument = self._old_definition.getInstrument()
         new_length = self._new_definition.getNumberHistograms()
         old_length = self._old_definition.getNumberHistograms()
 
-        # Check Instrument is correct
-        self.assertEquals(self._new_definition.getFullName(), 'VESUVIO')
+
         # Check number of detectors are equal
         self.assertEquals(new_length, old_length)
 
@@ -46,16 +47,16 @@ class CreateVesuvioIDFTest(unittest.TestCase):
             new_det = self._new_definition.getDetector(i)
             old_det = self._old_definition.getDetector(i)
             # Check for equality of z postions
-            self.assertEquals(round(new_det.getPos()[2], 5), round(old_det.getPos()[2], 5))
+            self.assertEquals(round(new_det.getPos().Z(), 5), round(old_det.getPos().Z(), 5))
             # Check for equality of IDs
             self.assertEquals(new_det.getID(), old_det.getID())
             # Check R values
-            new_r = new_det.getDistance(new_sample_pos)
-            old_r = old_det.getDistance(old_sample_pos)
-            self.assertEquals(new_r, old_r)
+            new_r = new_det.getDistance(new_instrument)
+            old_r = old_det.getDistance(old_instrument)
+            self.assertEquals(round(new_r,5), round(old_r,5))
             # Check Theta values
-            new_theta = ((self._new_definition.detectorTwoTheta(new_det) * 180) / math.pi)
-            old_theta = ((self._old_definition.detectorTwoTheta(old_det) * 180) / math.pi)
+            new_theta = round((self._new_definition.detectorTwoTheta(new_det) * 180) / math.pi, 4)
+            old_theta = round((self._old_definition.detectorTwoTheta(old_det) * 180) / math.pi, 4)
             self.assertEquals(new_theta, old_theta)
 
 
@@ -64,8 +65,8 @@ class CreateVesuvioIDFTest(unittest.TestCase):
         Test that the monitors are 0 and 1
         """
         # Get monitors
-        monitor_one = self._empty_instrument.getDetector(0)
-        monitor_two = self._empty_instrument.getDetector(1)
+        monitor_one = self._new_definition.getDetector(0)
+        monitor_two = self._new_definition.getDetector(1)
 
         # validator monitors
         # Check monitor
@@ -79,5 +80,5 @@ class CreateVesuvioIDFTest(unittest.TestCase):
         self.assertEquals(monitor_two.getPhi(), 0)
 
 
-    if __name__=="__main__":
-        unittest.main()
+if __name__=="__main__":
+    unittest.main()
