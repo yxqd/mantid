74882fe8c1ec512509b578c29e0419e578648507
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-04-04 11:29:54 +0100

    Refs #15812 Read out file size

--- scripts/SANS/SANSDataFileInfo.py
+++ scripts/SANS/SANSDataFileInfo.py
@@ -1,3 +1,4 @@
+
 from mantid.simpleapi import *
 
 
@@ -5,7 +6,6 @@ class SANSDataFileInfo(object):
     def __init__(self, workspace_name):
         super(SANSDataFileInfo, self).__init__()
         self._workspace_name = workspace_name
-        self._full_file_path = None
         self._history = None
         self._size = None
 
@@ -17,7 +17,6 @@ class SANSDataFileInfo(object):
 
     def _reset(self):
         self._workspace_name = None
-        self._full_file_path = None
         self._history = None
         self._size = None
 
@@ -25,8 +24,7 @@ class SANSDataFileInfo(object):
     def get_size_from_workspace(workspace_name):
         if mtd.doesExist(workspace_name):
             ws = mtd.retrieve(workspace_name)
-            return 0
-            # TODO: Finish getting size
+            return ws.getMemorySize()
         else:
             return 0
 
@@ -34,17 +32,7 @@ class SANSDataFileInfo(object):
     def get_history_from_workspace(workspace_name):
         if mtd.doesExist(workspace_name):
             ws = mtd.retrieve(workspace_name)
-            return ""
-            # TODO: Finish getting history
-        else:
-            return ""
-
-    @staticmethod
-    def get_full_file_path_from_workspace(workspace_name):
-        if mtd.doesExist(workspace_name):
-            ws = mtd.retrieve(workspace_name)
-            return ""
-            # TODO: Finish getting full file path
+            return str(ws.getHistory())
         else:
             return ""
 
@@ -59,18 +47,10 @@ class SANSDataFileInfo(object):
 
     def _workspace_is_still_the_same(self, requested_workspace_name):
         if self._workspace_name is None or not mtd.doesExist(self._workspace_name):
-            return (self._are_same_file_paths(requested_workspace_name) and
-                    self._are_same_compare_sizes(requested_workspace_name) and
-                    self._are_same_compare_histories(requested_workspace_name))
+            return (self._are_same_sizes(requested_workspace_name) and
+                    self._are_same_histories(requested_workspace_name))
         else:
             return True
-
-    def _are_same_file_paths(self, requested_workspace_name):
-        reference_file_path = self.get_full_file_path_from_workspace(requested_workspace_name)
-        if reference_file_path == self._full_file_path:
-            return True
-        else:
-            return False
 
     def _are_same_sizes(self, requested_workspace_name):
         reference_size = self.get_size_from_workspace(requested_workspace_name)
@@ -90,7 +70,9 @@ class SANSDataFileInfo(object):
     def is_same_workspace_name(workspace_name_1, workspace_name_2):
         return workspace_name_1 == workspace_name_2
 
+
 class SANSDataFileInfoStorage(object):
+
     def __init__(self):
         super(SANSDataFileInfoStorage, self).__init__()
         self.sample_sans = None
@@ -101,4 +83,10 @@ class SANSDataFileInfoStorage(object):
         self.can_direct = None
 
     def set_sample_sans(self, workspace_name):
+        pass
+
+    def set_sample_transmission(self, workspace_name):
+        pass
+
+    def set_sample_direct(self, workspace_name):
         pass
