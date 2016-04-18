beea60aa1db5bb47a82f7bd2949c1ccf10bb8be0
Anton Piccardo-Selg <anton.piccardo-selg@tessella.com>
2016-04-18 13:45:16 +0100

    Refs #15812 Further optimization work

--- scripts/SANS/SANSDataFileInfo.py
+++ scripts/SANS/SANSDataFileInfo.py
@@ -1,74 +1,202 @@
-﻿from mantid.simpleapi import *
+﻿import numbers
+from mantid.simpleapi import *
 
-class SANSDataFileInfo(object):
-    def __init__(self, workspace_name):
-        super(SANSDataFileInfo, self).__init__()
-        self._workspace_name = workspace_name
-        self._history = None
-        self._size = None
-        self._initialize()
 
-    def _initialize(self):
-        if self._workspace_name is not None and mtd.doesExist(self._workspace_name):
-            self._size = self.get_size_from_workspace(self._workspace_name)
-            self._history = self.get_history_from_workspace(self._workspace_name)
-        else:
-            self._reset()
 
-    def _reset(self):
+class SANSScatteringFileInfo(object):
+    def __init__(self):
+        super(SANSScatteringFileInfo, self).__init__()
+
+        # Workspace parameters
         self._workspace_name = None
         self._history = None
         self._size = None
 
-    @staticmethod
-    def get_size_from_workspace(workspace_name):
-        if mtd.doesExist(workspace_name):
-            ws = mtd.retrieve(workspace_name)
-            return ws.getMemorySize()
-        else:
-            return 0
+        # Dark Run parameters - this is the only case when we strictly require a reload
+        self._has_dark_run = None
 
-    @staticmethod
-    def get_history_from_workspace(workspace_name):
-        if mtd.doesExist(workspace_name):
-            ws = mtd.retrieve(workspace_name)
-            return str(ws.getHistory())
-        else:
-            return ""
+        # Tube Calibration paramters
+        self._tube_calibration_workspace_name = None
+        self._tube_calibration_file_path = None
 
-    def is_already_loaded(self, requested_workspace_name):
-        # If workspace name is None, or none is loaded or names don't match
-        if (self._workspace_name is None or not mtd.doesExist(self._workspace_name) or
-             not self._workspace_is_still_the_same(requested_workspace_name)):
+        # Geometry which is stored in sample
+        self._geometry_shape = None
+        self._geometry_thickness = None
+        self._geometry_width = None
+        self._geometry_height = None
+
+        # Beam Centre
+        self._beam_position_1 = None
+        self._beam_position_2 = None
+
+    @property
+    def workspace_name(self):
+        return self._workspace_name
+
+    @workspace_name.setter
+    def workspace_name(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("The workspace name must be a string")
+        self._workspace_name = value
+
+    @property
+    def history(self):
+        return self._history
+
+    @history.setter
+    def history(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("The history must be a string")
+        self._history = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, numbers.Integral):
+            raise ValueError("The size must be an integer.")
+        self._size = value
+
+    @property
+    def has_dark_run(self):
+        return self._has_dark_run
+
+    @has_dark_run.setter
+    def has_dark_run(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("The dark-run flag must be a bool.")
+        self._has_dark_run = value
+
+    @property
+    def tube_calibration_workspace_name(self):
+        return self._tube_calibration_workspace_name
+
+    @tube_calibration_workspace_name.setter
+    def tube_calibration_workspace_name(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("The calibration workspace name must be a string")
+        self._tube_calibration_workspace_name = value
+
+    @property
+    def tube_calibration_file_name(self):
+        return self._tube_calibration_workspace_name
+
+    @tube_calibration_file_name.setter
+    def tube_calibration_file_name(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("The calibration file path must be a string")
+        self._tube_calibration_file_name = value
+
+    @property
+    def geometry_shape(self):
+        return self._geometry_shape
+
+    @geometry_shape.setter
+    def geometry_shape(self, value):
+        if not isinstance(value, basestring):
+            raise ValueError("The geometry shape name must be a string")
+        self._geometry_shape = value 
+
+    @property
+    def geometry_thickness(self):
+        return self._geometry_thickness
+
+    @geometry_thickness.setter
+    def geometry_thickness(self, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError("The geometry thickness must be a real.")
+        self._geometry_thickness = value 
+
+    @property
+    def geometry_width(self):
+        return self._geometry_width
+
+    @geometry_width.setter
+    def geometry_width(self, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError("The geometry width must be a real.")
+        self._geometry_width = value 
+
+    @property
+    def geometry_height(self):
+        return self._geometry_height
+
+    @geometry_height.setter
+    def geometry_height(self, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError("The geometry width must be a real.")
+        self._geometry_height = value 
+
+    @property
+    def beam_centre_position_1(self):
+        return self._beam_position_1
+
+    @beam_centre_position_1.setter
+    def beam_centre_position_1(self, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError("The beam centre position 1 must be a real.")
+        self._beam_position_1 = value 
+
+    @property
+    def beam_centre_position_2(self):
+        return self._beam_position_2
+
+    @beam_centre_position_2.setter
+    def beam_centre_position_2(self, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError("The beam centre position 2 must be a real.")
+        self._beam_position_2 = value 
+
+    def is_in_valid_state(self):
+        if (self.workspace_name is None or
+            self.history is None or
+            self.size is None or 
+            self.has_dark_run is None or
+            self.geometry_shape is None or
+            self.geometry_thickness is None or
+            self.geometry_width is None or
+            self.geometry_height is None or
+            self.beam_position_1 is None or
+            self.beam_position_2 is None):
             return False
         else:
             return True
 
-    def _workspace_is_still_the_same(self, requested_workspace_name):
-        if self._workspace_name is not None and mtd.doesExist(self._workspace_name):
-            return (self.is_same_workspace_name(self._workspace_name, requested_workspace_name) and
-                    self._are_same_sizes(requested_workspace_name) and
-                    self._are_same_histories(requested_workspace_name))
-        else:
-            return False
+    def provide_parameter_hash(self):
+        # Tube Calibration paramters
+        tube_calibration_workspace_name = "" if self.tube_calibration_workspace_name is None else self.tube_calibration_workspace_name
+        tube_calibration_file_path = "" if self.tube_calibration_workspace_name is None else self.tube_calibration_workspace_name
 
-    def _are_same_sizes(self, requested_workspace_name):
-        reference_size = self.get_size_from_workspace(requested_workspace_name)
-        if reference_size == self._size:
+        param_hash = hash(tuple([self.workspace_name ,self.history, self.size, self.has_dark_run, 
+                                tube_calibration_workspace_name, tube_calibration_file_path,
+                                self.geometry_shape, self.geometry_thickness,
+                                self.geometry_width, self.geometry_height,
+                                self.beam_position_1, self.beam_position_2]))
+        return param_hash
+
+    def requires_a_reload(self, other_sans_data_file_info):
+        if not self.is_in_valid_state() or not other_sans_data_file_info.is_in_valid_state():
+            raise ValueError("The SANSScatterFileInfo is not in a valid state.")
+
+        # If a dark run has been performed then require a reload
+        if self.has_dark_run():
             return True
-        else:
-            return False
 
-    def _are_same_histories(self, requested_workspace_name):
-        reference_history = self.get_history_from_workspace(requested_workspace_name)
-        if reference_history == self._history:
+        # Compare parameter hash
+        param_hash = self.provide_parameter_hash()
+        param_hash_other = other_sans_data_file_info.provide_parameter_hash()
+
+        if param_hash == param_hash_other:
+            return False
+        else:
             return True
-        else:
-            return False
 
-    @staticmethod
-    def is_same_workspace_name(workspace_name_1, workspace_name_2):
-        return workspace_name_1 == workspace_name_2
+    def _compare_work_space_parametrs(self, other_sans_data_file_info):
+        return (self.workspace_name == other_sans_data_file_info.workspace_name and
+                self.size == other_sans_data_file_info.size and
+                self.history == other_sans_data_file_info.history)
 
 
 class SANSDataFileInfoStorage(object):
--- scripts/test/SANSDataFileInfoTest.py
+++ scripts/test/SANSDataFileInfoTest.py
@@ -1,33 +1,85 @@
 import unittest
 
 import mantid
-from mantid.api import (AlgorithmManager, mtd)
-from SANSDataFileInfo import SANSDataFileInfo
+from SANSDataFileInfo import SANSScatteringFileInfo
 
 
 SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME = "test_workspace_sans_data_file_info"
 
-def load_sample_data_set():
-    alg = AlgorithmManager.createUnmanaged("Load")
-    alg.initialize()
-    alg.setChild(True)
-    alg.setProperty("Filename", "LOQ48127.nxs")
-    alg.setProperty("OutputWorkspace", SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME)
-    alg.execute()
-    ws = alg.getProperty("OutputWorkspace").value
-    mtd.addOrReplace(SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME, ws)
-    return SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME
 
-def delete_sample_data_set():
-    if mtd.doesExist(SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME):
-        mtd.remove(SANS_DATA_FILE_INFO_SAMPLE_DATA_SET_NAME)
+class TestSANSScatteringFileInfo(unittest.TestCase):
+    def _setup_default_info(self, has_dark_run=False, is_valid=True):
+        info = SANSScatteringFileInfo()
 
+        info.workspace_name = "test_name"
+        info.history = "test_history"
+        info.size = 123
 
-class TestSANSDataFileInfo(unittest.TestCase):
-    def test_that_data_which_is_loaded_is_not_reloaded(self):
+        info.has_dark_run = has_dark_run
+
+        info.tube_calibration_workspace_name = "test_tube_name"
+        info.tube_calibration_file_name = "test_tube_file_name"
+
+        info.geometry_shape = "test_shape"
+        info.geometry_height = 123.3
+        info.geometry_thickness = 23.4
+        info.geometry_width = 1234.5
+
+        info.beam_centre_position_1 = 123.4
+        if is_valid:
+            info.beam_centre_position_2 = 34.5
+
+        return info
+
+    def test_that_properties_raise_if_incorrect_type_is_passed(self):
+        # Arrange
+        info = SANSScatteringFileInfo()
+
+        # Act + Assert
+        self.assertRaises(ValueError, setattr, info, "workspace_name", 123)
+        self.assertRaises(ValueError, setattr, info, "history", 123)
+        self.assertRaises(ValueError, setattr, info, "size", "123")
+
+        self.assertRaises(ValueError, setattr, info, "has_dark_run", "123")
+
+        self.assertRaises(ValueError, setattr, info, "tube_calibration_workspace_name", 123)
+        self.assertRaises(ValueError, setattr, info, "tube_calibration_file_name", 123)
+
+        self.assertRaises(ValueError, setattr, info, "geometry_shape", 123)
+        self.assertRaises(ValueError, setattr, info, "geometry_thickness", "123")
+        self.assertRaises(ValueError, setattr, info, "geometry_width", "123")
+        self.assertRaises(ValueError, setattr, info, "geometry_height", "123")
+
+        self.assertRaises(ValueError, setattr, info, "beam_centre_position_1", "123")
+        self.assertRaises(ValueError, setattr, info, "beam_centre_position_2", "123")
+
+    def test_old_file_with_dark_run_enabled_requires_reload(self):
+        # Arrange
+        info_old = self._setup_default_info(has_dark_run=True, is_valid=True)
+        info_new = self._setup_default_info(has_dark_run=False, is_valid=True)
+        # Act
+        requires_reload = info_old.requires_a_reload(info_new)
+        # Assert
+        self.assertTrue(requires_reload, "Having run a dark run should require a reload")
+
+    def test_only_new_file_with_dark_run_enabled_does_no_trequire_reload(self):
+        pass
+
+    def test_that_invalid_state_raises(self):
+        pass
+
+    def test_that_differing_parameter_hash_requires_reload(self):
+        pass
+
+    def test_that_same_parameter_hash_does_not_require_reload(self):
+        pass
+
+"""
+class TestSANSScatterinFileInfo(unittest.TestCase):
+    def test_that_incomplete_original_file_info_raises(self):
         # Arrange
         ws_name = load_sample_data_set()
-        ws_info = SANSDataFileInfo(ws_name)
+        ws_info = SANSScatterinFileInfo()
         # Act + Assert
         self.assertTrue(ws_info.is_already_loaded(ws_name), "Should detect as being already loaded")
         # Clean up
@@ -55,6 +107,6 @@ class TestSANSDataFileInfo(unittest.TestCase):
         self.assertFalse(ws_info.is_already_loaded(ws_name), "Should detect changed data set has to be reloaded")
         # Clean up
         delete_sample_data_set()
-
+"""
 if __name__ == "__main__":
     unittest.main()
