528ed5d6587883f2745d9fb579d3687a05ff1e56
Brendan Sullivan <sullivanbt@ornl.gov>
2018-06-22 10:50:22 -0400

    Re #22640 added unit test test file

---
+++ Testing/SystemTests/tests/analysis/IntegratePeaksProfileFitting.py
@@ -0,0 +1,26 @@
+#pylint: disable=no-init
+from __future__ import (absolute_import, division, print_function)
+import stresstesting
+from mantid.simpleapi import *
+
+
+class IntegratePeaksProfileFittingTest(stresstesting.MantidStressTest):
+    '''Tests the StepScan workflow algorithm'''
+
+    def runTest(self):
+        LoadMD(Filename='MANDI_5921_ProfileFitting.nxs', LoadHistory=False, OutputWorkspace='profile_fitting_test_md')
+        LoadNexus(Filename='MANDI_5921_ProfileFitting.integrate', OutputWorkspace='peaks_ws')
+        IntegratePeaksProfileFitting(OutputPeaksWorkspace='peaks_ws_out', OutputParamsWorkspace='params_ws', 
+                                     InputWorkspace='profile_fitting_test_md', PeaksWorkspace='peaks_ws', 
+                                     RunNumber=5921, UBFile='/SNS/MANDI/shared/ProfileFitting/demo_5921.mat',
+                                     ModeratorCoefficientsFile='/SNS/MANDI/shared/ProfileFitting/franz_coefficients_2017.dat', 
+                                     StrongPeakParamsFile='/SNS/MANDI/shared/ProfileFitting/strongPeakParams_beta_lac_mut_mbvg.pkl', 
+                                     PredPplCoefficients='3.56405,8.34072,0.141345', MinpplFrac=0.99, MaxpplFrac=1.01, DtSpread = 0.015)
+
+        intens3d = mtd['params_ws'].row(0)['Intens3d']
+        sigint3d = mtd['params_ws'].row(0)['SigInt3d']
+        self.assertDelta(intens3d, 280.19699, 0.01, "Incorrect intensity found.")
+        self.assertDelta(sigint3d, 20.168399, 0.01, "Incorrect intensity found.")
+
+    def validate(self):
+        return True
