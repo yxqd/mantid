c4e0b5ce6c063302192bf680fb8d7e25aa54e22c
Matthew Andrew <matthew.andrew@stfc.ac.uk>
2018-07-09 15:53:34 +0100

    Fixed override declaration Re #21657

--- qt/widgets/common/inc/MantidQtWidgets/Common/DataProcessorUI/QDataProcessorWidget.h
+++ qt/widgets/common/inc/MantidQtWidgets/Common/DataProcessorUI/QDataProcessorWidget.h
@@ -128,7 +128,7 @@ public:
   // Accessor methods
   std::map<int, std::set<int>> getSelectedChildren() const override;
   std::set<int> getSelectedParents() const override;
-  std::vector<int> getSelectedParentsVector() const override;
+  std::vector<int> getSelectedParentsVector() const;
   QString getProcessInstrument() const override;
   QString getWorkspaceToOpen() const override;
   QString getClipboard() const override;
